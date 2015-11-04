613a052fb8d5afc06472f77a1205750753c99607
Konrad Kühne <konrad.kuehne@rocketmail.com>
2015-11-04 17:24:52 +0100

    show static path

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var server = require("./src/server/core")
 var db = require("./src/server/database")
 
+
 var STATIC_PATH = __dirname + "/dist"
 var SERVER_PORT = 8090
+
+console.log("static:", STATIC_PATH)
 
 db.init()
 server.startServer(SERVER_PORT, STATIC_PATH)
